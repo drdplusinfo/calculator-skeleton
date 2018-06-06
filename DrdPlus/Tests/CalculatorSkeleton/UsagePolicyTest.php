85413c06cb6c17e92959da512908967751cc5db8
Jaroslav Týc <mail@jaroslavtyc.com>
2018-06-06 17:35:59 +0200

    Assets dirs merged

--- DrdPlus/Tests/CalculatorSkeleton/UsagePolicyTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace DrdPlus\Tests\CalculatorSkeleton;
-
-class UsagePolicyTest extends \DrdPlus\Tests\FrontendSkeleton\UsagePolicyTest
-{
-}
--- parts/calculator-skeleton/issues.php
+++ parts/calculator-skeleton/issues.php
@@ -3,10 +3,10 @@
     <div class="issues">
       <a href="https://rpgforum.cz/forum/viewtopic.php?f=238&t=14870">
         Máš nápad 😀? Vidíš chybu 😱?️ Sem s tím!
-        <img src="/images/generic/calculator-skeleton/rpgforum-ico.png">
+        <img src="/images/generic/skeleton/rpgforum-ico.png">
       </a>
       <a class="float-right" href="<?= $controller->getSourceCodeUrl() ?>"
-         title="Fork me on GitHub"><img class="github" src="/images/generic/calculator-skeleton/GitHub-Mark-64px.png"></a>
+         title="Fork me on GitHub"><img class="github" src="/images/generic/skeleton/GitHub-Mark-64px.png"></a>
     </div>
   </div>
 </div>
