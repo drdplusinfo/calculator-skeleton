d8e282c52c6f85e918b9d6ab73f4bcb032ff859e
Jaroslav Týc <mail@jaroslavtyc.com>
2018-06-12 08:25:32 +0200

    Space between history delete button and its description

--- parts/calculator-skeleton/history_deletion.php
+++ parts/calculator-skeleton/history_deletion.php
@@ -2,7 +2,7 @@
   <form class="col delete" action="/" method="post" onsubmit="return window.confirm('Opravdu smazat včetně historie?')">
     <label>
       <input type="submit" value="Smazat" name="<?= $controller::DELETE_HISTORY ?>" class="manual btn-danger">
-      <span class="hint">(včetně dlouhodobé paměti)</span>
     </label>
+    <span class="hint">(včetně dlouhodobé paměti)</span>
   </form>
 </div>
